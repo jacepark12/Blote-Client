30af8c6b1eebe6719d28f64e7fb6b62062c906d3
ParkJaesung <jspark141515@gmail.com>
2017-08-29 12:43:20 +0900

    Added static.css to app component style url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,7 @@ import * as web3 from 'Web3';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../assets/stylesheets/static.css']
 })
 export class AppComponent {
-  title = new web3().version.api;
 }
